9ed1fd5eecae9a17abdbd769673ca24d54362f13
Diego Parra <dparra@lctis.com.ve>
2018-05-22 08:16:56 -0400

    feat: fontawesome library added and finished index header finished

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ocho Estrellas</title>
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="./css/index.css">
+    <link href="./css/webfonts/fontawesome-all.css" rel="stylesheet">
+    <link rel="icon" href="./assets/waterfall.png">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li>
+                <a href="#inicio">Inicio</a>
+                <a href="#sobre-nosotros">Sobre Nosotros</a>
+                <a href="#explora">Explora</a>
+                <a href="#contactanos">Contactanos</a>
+                <a href="#reservaciones">Reservaciones</a>
+                <a href="#panel-administrativo">Panel Administrativo</a>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <a href="#login">Login</a>
+                <a href="#registrarse">Registrarse</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="header">
+        <div class="header-title-container">
+            <h1>Explora con nosotros!! </h1>
+            <h2>Hoteles en toda Venezuela</h2>
+        </div>
+
+        <div class="header-action-container">
+            <h1>
+                <a href="#reservaciones" class="reserve-button">
+                    Reservar Ahora <i class="fas fa-angle-right reserve-icon"></i>
+                </a>
+            </h1>
+        </div>
+    </div>
+
+    <header>
+        <h1>Ocho&nbsp;&nbsp;Estrellas</h1>
+        <div>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+    </header>
+
+    <section>
+        <h1>hash_algos</h1>
+    </section>
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
