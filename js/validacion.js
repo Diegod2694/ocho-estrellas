7b8edd0ed2cf05dd0957fb983d8d5c6d2410229e
jleodany <joseleodany@gmail.com>
2018-05-23 02:01:58 -0430

    Algo muy genial, que aprendí gracias a Diego Sanchez

--- js/validacion.js
+++ js/validacion.js
@@ -24,6 +24,29 @@ $(document).ready(function () {
     // });
 });
 
+// <script>
+//             function soloLetras(e){
+//                key = e.keyCode || e.which;
+//                tecla = String.fromCharCode(key).toLowerCase();
+//                letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
+//                especiales = "8-37-39-46";
+        
+//                tecla_especial = false
+//                for(var i in especiales){
+//                     if(key == especiales[i]){
+//                         tecla_especial = true;
+//                         break;
+//                     }
+//                 }
+        
+//                 if(letras.indexOf(tecla)==-1 && !tecla_especial){
+//                     return false;
+//                 }
+//             }
+//         </script>
+        
+//         <input type="text" onkeypress="return soloLetras(event)"> 
+
 function validateEmail(value) {
     console.log("Value => ", value);
     if (/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(value)) {
@@ -31,6 +54,8 @@ function validateEmail(value) {
     }
 }
 function validateForm() {
+    let errores = '';
+    document.getElementById('datosFaltantes').innerHTML = '';
     if ($('#nombreUsuario').val() == '') {
         document.getElementById("BtnEnviar").disabled = true;
     } else if ($('#apellidoUsuario').val() == '') {
@@ -52,4 +77,29 @@ function validateForm() {
     } else {
         document.getElementById("BtnEnviar").disabled = false;
     }
+    if ($('#nombreUsuario').val() == ''){
+        errores += '<li>Nombre</li>';
+    }
+    if ($('#apellidoUsuario').val() == ''){
+        errores += '<li>Apellido</li>';
+    }
+    if ($('#cedulaUsuario').val() == ''){
+        errores += '<li>Cédula</li>';
+    }
+    if ($('#user').val() == ''){
+        errores += '<li>Usuario</li>';
+    }
+    if ($('#email').val() == ''){
+        errores += '<li>Correo Electrónico</li>';
+    }
+    if (!/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test($('#email').val())){
+        errores += '<li>Correo Electrónico No Válido</li>'
+    }
+    if ($('#telfono').val() == ''){
+        errores += '<li>Teléfono</li>';
+    }
+    if ($('#direccion').val() == ''){
+        errores += '<li>Dirección</li>';
+    }
+    document.getElementById('datosFaltantes').innerHTML = errores;
 }
